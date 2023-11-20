1d59cd73d947f6e93286bfc3bd702719352388fe
MHP24 <mig.henriquezp@gmail.com>
2023-11-20 14:03:40 -0300

    update (client): message side cheking..

--- client/src/components/messages/messages-container.tsx
+++ client/src/components/messages/messages-container.tsx
@@ -4,26 +4,27 @@ import { useChat } from '../../hooks'
 export const MessagesContainer = () => {
   const { messages } = useChat()
 
-  // TODO: separate messages by type
-
   return (
     <ul className="flex flex-col gap-4 w-full h-full m-auto p-4">
-      {messages?.map(({ header, body, data, type }, i) => (
+      {messages?.map(({ header, body, data, type, side }, i) =>
         type !== 'input'
           ? <li key={`message-${i}`}>
+            {/* First message from bot response (should be a header) */}
             {header && <TextMessage side="system" text={header} />}
 
+            {/* The rest of messages ! Supported: text add ({ type }) to check */}
             {body && (
               <>
                 {body
-                // ! Check if has options to send the last message with options at the same time
+                  // ! Check if has options to send the last
+                  // ! message with options at the same time
                   .slice(0, data.option ? -1 : body.length)
                   .map(
                     ({ text }, j) =>
                       text && (
                         <TextMessage
                           key={`message-${i}-${j}`}
-                          side="system"
+                          side={side}
                           text={text}
                         />
                       )
@@ -31,6 +32,7 @@ export const MessagesContainer = () => {
               </>
             )}
 
+            {/* Options to select using buttons  */}
             {data.option && (
               <InteractiveMessage
                 options={data.option}
@@ -39,8 +41,15 @@ export const MessagesContainer = () => {
             )}
           </li>
 
-          : data.input?.input ? <TextMessage key={`input-message-${i}`} text={data.input?.input} side='system'/> : null
-      ))}
+        // ! Input messages = Simple text message...
+          : data.input?.input
+            ? <TextMessage
+              key={`input-message-${i}`}
+              text={data.input?.input}
+              side={side}
+            />
+            : null
+      )}
     </ul>
   )
 }
