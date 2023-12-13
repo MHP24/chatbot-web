48b0b041ba068af5af12750ad9cfc05293215d9c
MHP24 <mig.henriquezp@gmail.com>
2023-12-12 22:16:52 -0300

    update (server): .gitignore for static/ files generated from client

--- server/src/providers/prisma/mysql/migrations/20231129125308_01/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `data` on the `chats` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `chats` DROP COLUMN `data`,
-    ADD COLUMN `conversation` JSON NULL;
