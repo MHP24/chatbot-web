3dbfb8e8b30f78a1b0a5252beb1410eef1d5445f
MHP24 <mig.henriquezp@gmail.com>
2024-07-29 15:53:51 -0400

    fix: widget responsive

--- client/src/components/widget/widget-window.tsx
+++ client/src/components/widget/widget-window.tsx
@@ -10,7 +10,7 @@ export const WidgetWindow = () => {
   return (
     <div
       className={cn(
-        'flex flex-col fixed bottom-5 right-10 w-[23rem] h-[40rem] shadow-2xl rounded-xl z-20 bg-c1 text-c5 border-[1px] border-c6',
+        'flex flex-col fixed top-0 left-0 bottom-0 right-0 sm:bottom-5 sm:right-10 sm:left-auto sm:top-auto sm:w-[23rem] sm:h-[40rem] shadow-2xl sm:rounded-xl z-20 bg-c1 text-c5 border-[1px] border-c6',
         timesOpened < 1 ? 'hidden' : '',
         isOpen ? 'fadeIn' : 'fadeOut'
       )}
