f18702129e7c6070e389048528de2590d0661aac
MHP24 <mig.henriquezp@gmail.com>
2024-07-30 10:47:45 -0400

    feat: add side pictures

--- client/src/components/messages/interactive-message.tsx
+++ client/src/components/messages/interactive-message.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export const InteractiveMessage: FC<Props> = ({ header, options }) => {
   return (
-    <div className='flex flex-col items-center w-full m-auto bg-c1 rounded-xl shadow-sm border-[1px] border-c6'>
+    <div className='flex flex-col items-center w-full m-auto bg-c1 rounded-lg shadow-sm border-[1px] border-c6'>
       {header && <p className='text-center p-4 border-b-2 border-c6 w-full'>{header}</p>}
       <div className='p-2 bg-c1 w-full rounded-xl'>
         <ul className='flex flex-col gap-4 max-w-[80%] w-full m-auto py-2'>
